408fde231367cefbb577638587a3b055b77bb99f
jason1214 <jasontrabajo1@gmail.com>
2020-03-06 12:23:44 +0100

    Ignored git files and zipit zcript

--- zipit.sh
+++ zipit.sh
@@ -1,17 +1,2 @@
-#!/bin/bash 
-
-# printf "Please select folder:\n"
-# select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
-# zip -r prueba.zip $d
-# echo "Files added to prueba.zip"
-
-files = ( */ )
-for file in " ${files[@]} "; do
-    echo "$file"
-    zip -r prueba.zip $file
-done
-
-# LIST TODO
-
-# 1. install this script o call it on the work folder
-# 2. change folder dynamiclly
+git status --porcelain | while read modifiedFile; do 
+zip  -r prueba.zip ${modifiedFile:2} -x '*.git*''zipit.sh'; done
