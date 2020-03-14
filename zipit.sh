3eb4a679b76c35695769fcaa8c6d9a697b3991b9
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-14 23:59:49 +0100

    Added support for custom zip name

--- zipit.sh
+++ zipit.sh
@@ -1,3 +1,9 @@
-DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+DATE=$(date +"%Y%m%d%H%M%S")
+NAMEPROJECT=$(basename $(git rev-parse --show-toplevel))
+NAME="$NAMEPROJECT-$DATE"
+if [ -n "$1" ]
+    then
+        NAME="$1"
+fi
 git status --porcelain | while read modifiedFile; do 
-zip -r gitFiles"$DATE".zip ${modifiedFile:2} -x '*.git*' -x 'zipit.sh'; done 
+zip -r "$NAME".zip ${modifiedFile:2} -x '*.git*' -x 'zipit.sh'; done 
