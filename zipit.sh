6845493aeed67239eb1a60da08bec190032c56a2
JasonSteven14 <jasontrabajo1@gmail.com>
2020-03-14 22:39:28 +0100

    zipit_V-1.01

--- zipit.sh
+++ zipit.sh
@@ -1,2 +1,3 @@
+DATE=$(date +"%Y-%m-%d-%H-%M-%S")
 git status --porcelain | while read modifiedFile; do 
-zip  -r prueba.zip ${modifiedFile:2} -x '*.git*''zipit.sh'; done
+zip -r gitFiles"$DATE".zip ${modifiedFile:2} -x '*.git*' -x 'zipit.sh'; done 
